ba0f76fa093b9540d3977705269172d3ab41d524
Juan Pablo Patiño <82609508+JuanFlorez1326@users.noreply.github.com>
2023-02-27 13:33:29 -0500

    FINISH: Session Pipes I18nSelect and I18nPlural finished.

--- src/app/sales/pages/not-commons/not-commons.component.ts
+++ src/app/sales/pages/not-commons/not-commons.component.ts
@@ -4,4 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-not-commons',
   templateUrl: './not-commons.component.html'
 })
-export class NotCommonsComponent {}
+export class NotCommonsComponent {
+  
+  // i18nSelect
+  name: string = 'Susana';
+  gender: string = 'femenino';
+
+  invitationMap = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clients: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientsMap = {
+    '=0': '0 clientes esperando.',
+    '=1': '1 cliente esperando.',
+    'other': '# clientes esperando.'
+  }
+
+  changeClient() {
+    this.name = 'Pedro';
+    this.gender = 'masculino';
+  }
+
+  deleteClient() {
+    this.clients.pop();
+  }
+}
